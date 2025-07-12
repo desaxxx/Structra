271a71c8f62d58c3681f4ac7c24d520eb17d968a
desaxxx <0restart001@gmail.com>
2025-07-12 15:56:10 +0300

    Initialize

---
+++ src/main/java/com/desaoi/structra/Structra.java
@@ -0,0 +1,15 @@
+package com.desaoi.structra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Structra extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
