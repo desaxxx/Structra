085a047ed612d4f1f84c66c50f78ea99ad2236f8
desaxxx <0restart001@gmail.com>
2025-11-29 16:15:37 +0300

    feat: add Direction and Direction3D

---
+++ src/main/java/com/desoi/structra/direction/Direction.java
@@ -0,0 +1,77 @@
+package com.desoi.structra.direction;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * @since 1.0.1
+ */
+@Getter
+public class Direction {
+    public static final Direction P_X = new Direction((byte) 0); // 000
+    public static final Direction N_X = new Direction((byte) 4); // 100
+    public static final Direction P_Y = new Direction((byte) 1); // 001
+    public static final Direction N_Y = new Direction((byte) 5); // 101
+    public static final Direction P_Z = new Direction((byte) 2); // 010
+    public static final Direction N_Z = new Direction((byte) 6); // 110
+
+
+    private final byte encoded;
+    private Direction(byte encoded) {
+        this.encoded = (byte) (encoded & 7);
+    }
+
+    public boolean isX() {
+        return (encoded & 3) == 0;
+    }
+
+    public boolean isY() {
+        return (encoded & 3) == 1;
+    }
+
+    public boolean isZ() {
+        return (encoded & 3) == 2;
+    }
+
+    public boolean isPositive() {
+        return (encoded & 4) == 0;
+    }
+
+    public boolean isNegative() {
+        return (encoded & 4) != 0;
+    }
+
+    public Direction getOpposite() {
+        return new Direction((byte) (encoded ^ 4));
+    }
+
+    public boolean isSameAxis(Direction other) {
+        if(other == null) return false;
+        return (encoded & 3) == (other.encoded & 3);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction direction = (Direction) o;
+        return encoded == direction.encoded;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(encoded);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Direction[%d]", encoded);
+    }
+
+    @NotNull
+    public static Direction of(byte encoded) {
+        return new Direction(encoded);
+    }
+}
---
+++ src/main/java/com/desoi/structra/direction/Direction3D.java
@@ -0,0 +1,78 @@
+package com.desoi.structra.direction;
+
+import com.desoi.structra.util.Validate;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * @see Direction
+ * @since 1.0.1
+ */
+@Getter
+public class Direction3D {
+
+    /*
+     * fff -> first direction encoded
+     * sss -> second direction encoded
+     * ttt -> third direction encoded
+     * Direction3D encoded: 0b0000 0fff 0sss 0ttt
+     */
+    private final int encoded;
+
+    private Direction3D(int encoded) {
+        this.encoded = (byte) (encoded & 4095);
+    }
+
+    /**
+     * @return First direction
+     * @since 1.0.1
+     */
+    public Direction first() {
+        return Direction.of((byte) ((encoded >> 8) & 7));
+    }
+
+    /**
+     * @return Second direction
+     * @since 1.0.1
+     */
+    public Direction second() {
+        return Direction.of((byte) ((encoded >> 4) & 7));
+    }
+
+    /**
+     * @return Third direction
+     * @since 1.0.1
+     */
+    public Direction third() {
+        return Direction.of((byte) (encoded & 7));
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction3D that = (Direction3D) o;
+        return encoded == that.encoded;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(encoded);
+    }
+
+    @NotNull
+    public static Direction3D of(@NotNull Direction first, @NotNull Direction second, @NotNull Direction third) {
+        Validate.notNull(first, "First direction cannot be null.");
+        Validate.notNull(second, "Second direction cannot be null.");
+        Validate.notNull(third, "Third direction cannot be null.");
+        Validate.validate(!first.isSameAxis(second) && !first.isSameAxis(third) && !second.isSameAxis(third), "Directions cannot be on same axis.");
+
+        byte value = 0;
+        value |= (byte) (first.getEncoded() << 8);
+        value |= (byte) (second.getEncoded() << 4);
+        value |= third.getEncoded();
+        return new Direction3D(value);
+    }
+}
