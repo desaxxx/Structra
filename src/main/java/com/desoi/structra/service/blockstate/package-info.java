76639250564a5d02b35df7f20220d922600406c5
desaxxx <0restart001@gmail.com>
2025-11-30 14:02:31 +0300

    docs: add package info for blockstate

---
+++ src/main/java/com/desoi/structra/service/blockstate/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package handles BlockStates as listed at
+ * <a href="https://jd.papermc.io/paper/1.21.10/org/bukkit/block/BlockState.html">Paper Javadoc</a>
+ *
+ * <p>Latest supported version <b>Paper 1.21.10</b></p>
+ * <p>Spigot and Bukkit is not supported as the plugin handles Paper specific classes.</p>
+ *
+ */
+package com.desoi.structra.service.blockstate;
